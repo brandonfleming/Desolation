b54fd94f60f67cf384b47f960ccb6409730ab8c7
brandonfleming <41931266+brandonfleming@users.noreply.github.com>
2018-12-05 14:27:43 -0600

    New in Desolation v0.3.0

---
+++ AmbienceController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbienceController : MonoBehaviour {
+
+    public AudioClip Ambience1, Ambience2;
+    public AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+
+        source.Play();
+
+	}
+
+    public void Update()
+    {
+        source.clip = Ambience1;
+    }
+
+
+}
---
+++ PauseScreen.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour {
+
+    public GameObject PauseUI;
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+        PauseUI.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+	}
+
+    
+   void Pause ()
+    {
+        Time.timeScale = 0f;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Player.GetComponent<FirstPersonController>().enabled = false;
+        PauseUI.SetActive(true);
+        Debug.Log("IsPaused");
+    }
+
+    public void Resume ()
+    {
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Player.GetComponent<FirstPersonController>().enabled = true;
+        PauseUI.SetActive(false);
+        Debug.Log("IsResuming");
+    }
+
+    public void MainMenu ()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
